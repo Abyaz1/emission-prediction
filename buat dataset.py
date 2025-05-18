82b8d4407ce9fca3f7610094e1c982cf8e995a6c
Abyaz <149954344+Abyaz1@users.noreply.github.com>
2025-05-18 21:08:33 +0700

    Update buat dataset.py

--- buat dataset.py
+++ buat dataset.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Membaca dataset (sesuaikan dengan path dataset Anda)
 # Misalnya, dataset disimpan dalam file CSV
 data = pd.read_csv('large_dataset_1M_rows.csv')
 
-# Menentukan ukuran dataset yang lebih kecil (contoh: 20% dari dataset asli)
+# ukuran dataset yang diinginkan dalam persen
 subset_size = 0.25
 
 # Membagi dataset menjadi subset (20%) dan sisanya (80%)
@@ -16,4 +15,4 @@ print(f"Jumlah data subset: {len(subset)}")
 print(subset.head())
 
 # Menyimpan subset ke file baru jika diperlukan
-subset.to_csv('dataset 250k.csv', index=False)
+subset.to_csv('dataset 25%.csv', index=False)
